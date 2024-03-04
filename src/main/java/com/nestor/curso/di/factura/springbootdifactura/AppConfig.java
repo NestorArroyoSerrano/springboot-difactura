005da779dadd64dfe04ef77219ca6be020ebcd2c
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-03-04 09:25:28 +0100

    Contextos (Scope)

--- src/main/java/com/nestor/curso/di/factura/springbootdifactura/AppConfig.java
+++ src/main/java/com/nestor/curso/di/factura/springbootdifactura/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.nestor.curso.di.factura.springbootdifactura.models.Item;
@@ -14,11 +15,20 @@ import com.nestor.curso.di.factura.springbootdifactura.models.Product;
 @PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class AppConfig {
     
-    @Bean
+    @Bean("default")
+   //  @Primary
     List<Item> itemsInvoice(){
-
        return Arrays.asList(new Item(new Product("Camara Sony", 800), 2), new Item(new Product("Bicicleta Bianchi 26", 1200), 4));
+    }
 
+    @Bean
+    List<Item> itemsInvoiceOffice(){
+    Product p1 = new Product("Monitor Asus 24", 700);
+    Product p2 = new Product("Notebook Razer", 2400);
+    Product p3 = new Product("Impresora HP", 800);
+    Product p4 = new Product("Escritorio Oficina", 900);
+
+    return Arrays.asList(new Item(p1, 2), new Item(p2,6), new Item(p3, 1), new Item(p4,4));
        
     }
 }
